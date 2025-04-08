9d3f5c3d842c273aa4a5b6966243fac4fa698df0
MurilloCaravita <66228176+MurilloCaravita@users.noreply.github.com>
2025-04-07 23:48:19 -0300

    Delete grafico prototipo PI directory

--- grafico prototipo PI/projeto_integrador-main/bd.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as conexao
-
-banco = conexao.connect(
-    host = "localhost",
-    user = "root",
-    password = "murillo123",
-    database= "sustentabilidade",
-    autocommit=True
-    )
--- grafico prototipo PI/projeto_integrador-main/tabelacadastro.sql
+++
@@ -1,9 +0,0 @@
-create table cadastro(
-	id integer auto_increment primary key not null unique,
-    consumo float not null,
-    nr float not null,
-    energia float not null,
-    trans varchar(30) not null
-    )
-    
-#select * from cadastro;
